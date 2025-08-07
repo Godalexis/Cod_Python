ad8dd465efeedcdd998599c67cfc28f3b5488b19
Alexis <sorattialexis@gmail.com>
2025-08-07 11:24:19 -0300

    guardando cambios

--- create_files.py
+++ create_files.py
@@ -2,7 +2,7 @@ class TxtRepository:
     def __init__(self, archivo: str = "data.txt"):
         self.archivo = archivo 
 
-    def save_data (self, data: dict) -> bool:
+    def  save_data (self, data: dict) -> bool:
         try:
             linea = "#".join([f"{k}:{v}" for k, v in data.items()])
             with open(self.archivo, "a") as archivo:
@@ -10,18 +10,28 @@ class TxtRepository:
             return True
         except Exception as e:
             print(f"Error al guardar los datos: {e}")
-            return False
+            return False   
         
+    def busqueda_por_concepto(self, concepto: str) -> str:
+        with open(self.archivo, "r") as archivo:
+            for linea in archivo:
+                if f"concepto:{concepto}" in linea:
+                    return linea.strip()
+        return "No se encontró el concepto."
+    
 repo = TxtRepository()
 
 datos = {
-    "nombre": "Lucas",
-    "edad": 25,
-    "ciudad": "Corrientes"
-}
+    "nombre": "Juan",
+    "edad": 28,
+    "ciudad": "Jujuy"
+    }
 
 exito = repo.save_data(datos)
 if exito:
     print("Datos guardados correctamente.")
 else:
     print("No se pudieron guardar los datos.")
+
+resultado = repo.busqueda_por_concepto("Gaseosa")
+print(resultado)
